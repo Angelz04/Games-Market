2daf224c7272a290f337729f7994b2f8368697b0
Angelz04 <angelosbh2004@gmail.com>
2024-03-09 13:34:04 -0500

    Ajuste logo

--- proyectangel/src/Components/Logo/Logo.jsx
+++ proyectangel/src/Components/Logo/Logo.jsx
@@ -2,7 +2,7 @@ import './Logo.css'
 const Logo = () => {
     return (
     <div>
-        <img className='logo' src="../src/assets/Logo.png" alt="Logo png" />
+        <img className='logo' src="https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/bltead1f4cb9ad32e19/60d20208bcec595109d831c2/X_NullCmd.png" alt="Logo png" />
     </div>
 )
 }
